aab4f018a5b4db332987d1c698628378d8d72496
Vuzi <vuzi@free.fr>
2016-05-29 00:27:38 +0200

    Service scheduling + store

---
+++ NetworkManagerService/NetworkManagerService/Service1.cs
@@ -0,0 +1,53 @@
+﻿using NetworkManager.Scheduling;
+using System;
+using System.Data.SQLite;
+using System.Linq;
+using System.ServiceProcess;
+
+namespace NetworkManagerService {
+    public partial class Service1 : ServiceBase {
+        public Service1() {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args) {
+            Console.WriteLine("hello world");
+
+            Task task = new Task() {
+                UUID = Guid.NewGuid().ToString(),
+                date = DateTime.Now,
+                type = TaskType.INSTALL_SOFTWARE,
+                value = "Firefox",
+                extra = "-ms" 
+            };
+            
+            SQLiteConnection.CreateFile("NetworkManager.sqlite");
+            var conn = new SQLiteConnection("Data Source=NetworkManager.sqlite;Version=3;");
+            conn.Open();
+            var taskStore = new TaskStore(conn);
+
+            taskStore.insertTask(task);
+
+            TaskResult taskResult = new TaskResult() {
+                UUID = Guid.NewGuid().ToString(),
+                task = task,
+                computerName = "computerName",
+                domainName = "domainName",
+                status = TaskStatus.SCHEDULED
+            };
+
+            taskStore.insertTaskResult(taskResult);
+
+            taskResult = taskStore.getTasksResultByTask(task).FirstOrDefault();
+
+            Console.WriteLine("hello world");
+        }
+
+        protected override void OnStop() {
+        }
+
+        public void Start(string[] args) {
+            OnStart(args);
+        }
+    }
+}
---
+++ NetworkManagerService/NetworkManagerService/src/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetworkManagerService {
+    static class Program {
+        /// <summary>
+        /// Point d'entrée principal de l'application.
+        /// </summary>
+        static void Main(string[] args) {
+            Service1 service = new Service1();
+
+            if (Environment.UserInteractive) {
+                service.Start(args);
+                Console.WriteLine("Press any key to stop program");
+                Console.Read();
+                service.Stop();
+            } else {
+                ServiceBase.Run(service);
+            }
+        }
+    }
+}
